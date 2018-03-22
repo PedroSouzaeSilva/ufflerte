cf541cd555e123644d01b555da17d80c3ee580c4
PC <brunovx6@gmail.com>
2018-03-22 18:35:52 -0300

    adicionando tela de apresentação

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -18,50 +18,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class LoginPage {
 
   splash = true;
-  
-  
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    window.onload = function(){
-
-      myMoveRight(),
-      myMoveLeft();
-      
-    }
-  
-  
-  function myMoveRight() {
-    var elem = document.getElementById("logo4");   
-    var pos = 0;
-    var idRight = setInterval(frameRight, 14);
-    function frameRight() {
-      if (pos >= 137) {
-        clearInterval(idRight);
-      }
-      else {
-        pos++; 
-        elem.style.left = pos + 'px'; 
-      }
-    }
-  }
-  
-  
-  
-  function myMoveLeft() {
-      var elem = document.getElementById("logo5");   
-      var pos = 360;
-      var idLeft = setInterval(frameLeft, 14);
-      function frameLeft() {
-          if (pos <= 270) {
-            clearInterval(idLeft);
-          }
-          else {
-            pos--; 
-            elem.style.left = pos + 'px'; 
-          }
-        
-      }
-    }
   }
 
   ionViewDidLoad() {
