b0a22c279c6faef418655c3be5008406766b7e02
PC <brunovx6@gmail.com>
2018-03-25 01:34:02 -0300

    pagina meu-perfil faltando detalhes de sombra em botoes e linha vertical

---
+++ src/pages/meu-perfil/meu-perfil.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+
+/**
+ * Generated class for the MeuPerfilPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-meu-perfil',
+  templateUrl: 'meu-perfil.html',
+})
+export class MeuPerfilPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MeuPerfilPage');
+  }
+  nextPage(){
+    this.navCtrl.push(LoginPage);
+  }
+
+}
